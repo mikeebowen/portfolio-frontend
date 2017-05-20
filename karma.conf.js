e559373af79d55cd402cb70322a6888fa94c0061
Michael Bowen <mikee.bowen@gmail.com>
2017-05-20 11:45:52 -0700

    add style and testing tools
    add bootstrap, background image and configure karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['Chromium'],
+    singleRun: true
   });
 };
