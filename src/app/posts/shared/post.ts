436e11efaf949cb0324b044a854d29b4222344b4
Michael Bowen <mikee.bowen@gmail.com>
2017-07-01 12:35:27 -0700

    feat(Post): add description, image, and imageDescription to Post

--- src/app/posts/shared/post.ts
+++ src/app/posts/shared/post.ts
@@ -1,22 +1,31 @@
 interface IPostOptions {
   title?: string;
   subtitle?: string;
-  content?: string;
   author?: string;
+  description?: string;
+  content?: string;
+  image?: string;
+  imageDescription?: string;
 }
 
 export class Post {
   title: string;
   subtitle: string;
-  content: string;
   author: string;
+  description: string;
+  content: string;
+  image: string;
+  imageDescription: string;
   uniqueTitle: string;
 
   constructor(options: IPostOptions) {
     this.title = options.title;
-    this.subtitle = options.title;
-    this.content = options.content;
+    this.subtitle = options.subtitle;
     this.author = options.author;
+    this.description = options.description;
+    this.content = options.content;
+    this.image = options.image;
+    this.imageDescription = options.imageDescription;
     // TODO add timestamp for unique title in database
     this.uniqueTitle = this.title.toLowerCase().trim().replace(/\s/g, '-')/* + Date.now().toString()*/;
   }
