5c1c020f0ea246e3cc9daee568893442a32ee697
Michael Bowen <mikee.bowen@gmail.com>
2017-07-01 12:36:26 -0700

    feat(PostListComponent): get posts from data
    post list now comes from data and is displayed in dom through template binding

--- src/app/posts/posts-list/posts-list.component.ts
+++ src/app/posts/posts-list/posts-list.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import posts from '../../../assets/posts';
+import { Post } from '../shared/post';
+
 @Component({
   selector: 'app-posts-list',
   templateUrl: './posts-list.component.html',
   styleUrls: [ './posts-list.component.scss' ]
 })
 export class PostsListComponent implements OnInit {
+  posts: Post[];
 
   constructor() { }
 
   ngOnInit() {
+    this.posts = posts;
   }
 
 }
