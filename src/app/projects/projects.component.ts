68b69de68a62d7efd2e683e4553dd81ba1b86e7e
Michael Bowen <mikee.bowen@gmail.com>
2017-07-03 22:20:30 -0700

    feat(ProjectsComponent): get projects from data

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { slideInOut } from '../shared/animations/index';
 
+import projects from '../../assets/projects';
+import { Post } from '../shared/classes/post';
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: [ './projects.component.scss' ],
   animations: [ slideInOut ],
   // tslint:disable-next-line:use-host-property-decorator
-  host: { '[@slideInOut]': '' }
+  // host: { '[@slideInOut]': '' }
 })
 export class ProjectsComponent implements OnInit {
-
+  projects: Post[];
   constructor() { }
 
   ngOnInit() {
+    this.projects = projects;
   }
 
 }
