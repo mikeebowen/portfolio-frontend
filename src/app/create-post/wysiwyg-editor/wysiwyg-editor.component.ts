565529e71bb584c2e09f63ae5a359ff06e6652b3
Michael Bowen <mikee.bowen@gmail.com>
2017-07-13 22:33:21 -0700

    feat(WysiwygEditorComponent): add tinymce
    adds images as base64 strings and outputs post as html

--- src/app/create-post/wysiwyg-editor/wysiwyg-editor.component.ts
+++ src/app/create-post/wysiwyg-editor/wysiwyg-editor.component.ts
@@ -13,13 +13,19 @@ import 'tinymce/themes/modern';
 
 import 'tinymce/plugins/table';
 import 'tinymce/plugins/link';
+import 'tinymce/plugins/image';
+import 'tinymce/plugins/spellchecker';
+import 'tinymce/plugins/imagetools';
+import 'tinymce/plugins/save';
+import 'tinymce/plugins/lists';
+import 'tinymce/plugins/codesample';
 
 declare const tinymce: any;
 
 @Component({
   selector: 'app-wysiwyg-editor',
   templateUrl: './wysiwyg-editor.component.html',
-  styleUrls: [ './wysiwyg-editor.component.scss' ]
+  styleUrls: ['./wysiwyg-editor.component.scss']
 })
 export class WysiwygEditorComponent implements OnInit, OnDestroy, AfterViewInit {
   @Input() elementId: string;
@@ -35,16 +41,61 @@ export class WysiwygEditorComponent implements OnInit, OnDestroy, AfterViewInit 
   ngAfterViewInit() {
     tinymce.init({
       selector: '#' + this.elementId,
-      plugins: [ 'link', 'table' ],
+      plugins: ['link', 'table', 'spellchecker', 'image', 'imagetools', 'save', 'lists', 'imagetools', 'codesample'],
+      // tslint:disable-next-line:max-line-length
+      toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image codesample | save',
+      menubar: 'file edit insert view format table tools save image',
       min_height: 400,
       skin_url: 'assets/skins/lightgray',
+      browser_spellcheck: true,
+      codesample_languages: [
+        { text: 'HTML/XML', value: 'markup' },
+        { text: 'JavaScript', value: 'javascript' },
+        { text: 'CSS', value: 'css' },
+        { text: 'PHP', value: 'php' },
+        { text: 'Ruby', value: 'ruby' },
+        { text: 'Python', value: 'python' },
+        { text: 'Java', value: 'java' },
+        { text: 'C', value: 'c' },
+        { text: 'C#', value: 'csharp' },
+        { text: 'C++', value: 'cpp' }
+      ],
       setup: editor => {
         this.editor = editor;
         editor.on('keyup change', () => {
           const content = editor.getContent();
           this.onEditorContentChange.emit(content);
         });
-      }
+      },
+      image_title: true,
+      file_browser_callback_types: 'image',
+      file_picker_callback: function (callback, value, meta) {
+        const input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+
+        input.onchange = (function (e: any) {
+          const file = e.path[0].files[0];
+          const reader = new FileReader();
+
+          reader.readAsDataURL(file);
+
+          reader.onload = function () {
+            const id = 'blobid' + (new Date()).getTime();
+            const blobCache = tinymce.activeEditor.editorUpload.blobCache;
+            const base64 = reader.result.split(',')[1];
+            const blobInfo = blobCache.create(id, file, base64);
+            blobCache.add(blobInfo);
+
+            callback(blobInfo.blobUri(), { title: file.name });
+          };
+        });
+        input.click();
+      },
+      // TODO add save method
+      save_onsavecallback: function () { console.log('Saved'); },
+      // TODO figure out why cancel button isn't showing
+      save_oncancelcallback: function () { console.log('Save canceled'); }
     });
   }
 
