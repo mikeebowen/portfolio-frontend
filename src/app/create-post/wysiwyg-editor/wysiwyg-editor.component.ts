c1622b546f3c0df83dcf86bf7ad8eb9e72903b12
Michael Bowen <mikee.bowen@gmail.com>
2017-07-19 22:15:16 -0700

    feat(WysiwygEditorComponent): move methods to named methods
    also add modal for confirming save

--- src/app/create-post/wysiwyg-editor/wysiwyg-editor.component.ts
+++ src/app/create-post/wysiwyg-editor/wysiwyg-editor.component.ts
@@ -5,12 +5,11 @@ import {
   EventEmitter,
   OnDestroy,
   Input,
-  Output
+  Output, ViewChild
 } from '@angular/core';
 
 import 'tinymce';
 import 'tinymce/themes/modern';
-
 import 'tinymce/plugins/table';
 import 'tinymce/plugins/link';
 import 'tinymce/plugins/image';
@@ -19,9 +18,14 @@ import 'tinymce/plugins/imagetools';
 import 'tinymce/plugins/save';
 import 'tinymce/plugins/lists';
 import 'tinymce/plugins/codesample';
+import 'tinymce/plugins/code';
+
 import { BlogPostsService } from '../../shared/services/blog-posts.service';
+import { ModalDirective } from 'ngx-bootstrap';
 
 declare const tinymce: any;
+let fileLink: string;
+
 
 @Component({
   selector: 'app-wysiwyg-editor',
@@ -32,6 +36,41 @@ export class WysiwygEditorComponent implements OnInit, OnDestroy, AfterViewInit 
   @Input() elementId: string;
   @Output() onEditorContentChange = new EventEmitter();
   editor: any;
+  @ViewChild('saveOrCancelModal') saveOrCancelModal: ModalDirective;
+  file_upload_callback: Function = (callback, value, meta) => {
+    const component = this;
+    const input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+
+    input.onchange = (function (e: any) {
+      const file = e.path[0].files[0];
+      const reader = new FileReader();
+
+      reader.readAsDataURL(file);
+
+      reader.onload = function () {
+        const id = 'blobid' + (new Date()).getTime();
+        const blobCache = tinymce.activeEditor.editorUpload.blobCache;
+        const base64 = reader.result.split(',')[1];
+        const blobInfo = blobCache.create(id, file, base64);
+        blobCache.add(blobInfo);
+
+        component.uploadFile(base64, file.name, () => {
+          callback(fileLink, { title: file.name });
+        });
+      };
+    });
+    input.click();
+  }
+
+  tinymce_setup: Function = editor => {
+    this.editor = editor;
+    editor.on('keyup change', () => {
+      const content = editor.getContent();
+      this.onEditorContentChange.emit(content);
+    });
+  }
 
   constructor(private blogPostsService: BlogPostsService) {
   }
@@ -43,11 +82,11 @@ export class WysiwygEditorComponent implements OnInit, OnDestroy, AfterViewInit 
 
     tinymce.init({
       selector: '#' + this.elementId,
-      plugins: ['link', 'table', 'spellchecker', 'image', 'imagetools', 'save', 'lists', 'imagetools', 'codesample'],
+      plugins: ['link', 'table', 'spellchecker', 'image', 'imagetools', 'save', 'lists', 'imagetools', 'codesample', 'code '],
       // tslint:disable-next-line:max-line-length
-      toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image codesample | save',
-      menubar: 'file edit insert view format table tools save image',
-      min_height: 400,
+      toolbar: 'insertfile undo redo | code | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image codesample | save cancel',
+      menubar: 'file edit insert view format table tools image',
+      min_height: 500,
       skin_url: 'assets/skins/lightgray',
       browser_spellcheck: true,
       codesample_languages: [
@@ -62,44 +101,17 @@ export class WysiwygEditorComponent implements OnInit, OnDestroy, AfterViewInit 
         { text: 'C#', value: 'csharp' },
         { text: 'C++', value: 'cpp' }
       ],
-      setup: editor => {
-        this.editor = editor;
-        editor.on('keyup change', () => {
-          const content = editor.getContent();
-          this.onEditorContentChange.emit(content);
-        });
-      },
+      setup: this.tinymce_setup,
       image_title: true,
       file_browser_callback_types: 'image',
-      file_picker_callback: (callback, value, meta) => {
-        const component = this;
-        const input = document.createElement('input');
-        input.setAttribute('type', 'file');
-        input.setAttribute('accept', 'image/*');
-
-        input.onchange = (function (e: any) {
-          const file = e.path[0].files[0];
-          const reader = new FileReader();
-
-          reader.readAsDataURL(file);
-
-          reader.onload = function () {
-            const id = 'blobid' + (new Date()).getTime();
-            const blobCache = tinymce.activeEditor.editorUpload.blobCache;
-            const base64 = reader.result.split(',')[1];
-            const blobInfo = blobCache.create(id, file, base64);
-            blobCache.add(blobInfo);
-            component.uploadFile(base64, file.name);
-
-            callback(blobInfo.blobUri(), { title: file.name });
-          };
-        });
-        input.click();
-      },
+      file_picker_callback: this.file_upload_callback,
       // TODO add save method
-      save_onsavecallback: function () { console.log('Saved'); },
-      // TODO figure out why cancel button isn't showing
-      save_oncancelcallback: function () { console.log('Save canceled'); }
+      save_onsavecallback: () => {
+        // alert('TODO add save method');
+        this.saveOrCancelModal.show();
+      },
+      // TODO add cancel method
+      save_oncancelcallback: () => { alert('TODO add cancel method'); }
     });
   }
 
@@ -107,9 +119,12 @@ export class WysiwygEditorComponent implements OnInit, OnDestroy, AfterViewInit 
     tinymce.remove(this.editor);
   }
 
-  uploadFile(toBase64String: any, fileName: string) {
+  uploadFile(toBase64String: any, fileName: string, callback: Function) {
     this.blogPostsService.uploadFile(toBase64String, fileName).subscribe(
-      res => console.log('res : ', res),
+      (res: string) => {
+        fileLink = res;
+        callback();
+      },
       err => console.error('error uploading file : ', err)
     );
   }
