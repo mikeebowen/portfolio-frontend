163f1540dfd08e3e84c9b3dc04e8c63c8e60aa7c
Michael Bowen <mikee.bowen@gmail.com>
2017-10-01 15:14:27 -0700

    add contact page

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,14 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: [ './contact.component.scss' ]
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  emailSubject: string;
+  emailContent: string;
 
   constructor() {
   }
 
   ngOnInit() {
+  }
+
+  sendEmail() {
+    window.open(`mailto:mikee.bowen@gmail.com?subject=${this.emailSubject}&body=${this.emailContent}`, '_self');
   }
 
 }
